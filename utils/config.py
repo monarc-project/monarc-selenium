3b84cb9258519bcff5db457c74a548179a7de074
Ruslan Baidan <ruslan.baidan@nc3.lu>
2023-04-27 16:44:58 +0200

    Initial commit contains login page test suites.

---
+++ utils/config.py
@@ -0,0 +1,11 @@
+class Config:
+    """Config class to store configuration variables"""
+
+    # Selenium configuration
+    BASE_URL = "https://my.monarc.lu/casesmodels2/"
+
+    # Login configuration
+    VALID_USERNAME = ""
+    VALID_PASSWORD = ""
+    INVALID_USERNAME = "invalid_username"
+    INVALID_PASSWORD = "invalid_password"
